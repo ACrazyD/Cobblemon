1192e2a6275ee7909fd7b84eda490f3f6aeef19d
ACrazyD <41309569+ACrazyD@users.noreply.github.com>
2023-04-01 21:56:32 -0700

    Updates

---
+++ kubejs/server_scripts/mods.js
@@ -0,0 +1,4 @@
+let MC = (id) => 'minecraft:'+id
+let KJ = (id) => 'kubejs:'+id
+let CM = (id) => 'cobblemon:'+id
+let LMC = (id) => 'lightmanscurrency:'+id
--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -3,9 +3,16 @@
 console.info('Hello, World! (You will only see this line once in console, during startup)')
 
 StartupEvents.registry('item', event => {
+	var pokeballs = ['poke','citrine','verdant','azure','roseate','slate','premier','great','ultra','safari','fast','level','lure','heavy','love','friend','moon','sport','park','net','dive','nest','repeat','timer','luxury','dusk','heal','quick','dream','beast','master','cherish']
+
+    pokeballs.forEach(f => {
+        event.create(`uncharged_${f}_ball`).texture(`cobblemon:items/poke_balls/${f}_ball`).color(0, 0x4f5359);
+        event.create(`incomplete_${f}_ball`, `create:sequenced_assembly`).texture(`cobblemon:items/poke_balls/${f}_ball`).color(0, 0x222324);
+    });
+})
+
 	// Register new items here
 	// event.create('example_item').displayName('Example Item')
-})
 
 StartupEvents.registry('block', event => {
 	// Register new blocks here
