cf6b24b05ea18a47552567e105ab961b5b09fb17
ACrazyD <41309569+ACrazyD@users.noreply.github.com>
2023-04-03 13:58:18 -0700

    Updates/Tweaks - Step Closer to fixing a Crash.

--- kubejs/server_scripts/mods.js
+++ kubejs/server_scripts/mods.js
@@ -2,3 +2,5 @@ let MC = (id) => 'minecraft:'+id
 let KJ = (id) => 'kubejs:'+id
 let CM = (id) => 'cobblemon:'+id
 let LMC = (id) => 'lightmanscurrency:'+id
+let CC = (id) => 'computercraft:'+id
+let APO = (id) => 'apotheosis:'+id
