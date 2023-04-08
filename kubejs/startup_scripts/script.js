26c22dac66283b8150658c8e50e2e44aea2f18e3
ACrazyD <41309569+ACrazyD@users.noreply.github.com>
2023-04-08 16:12:26 -0700

    Adding Items (Broken atm)

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -3,21 +3,43 @@
 console.info('MORE ITEMS/BLOCKS!!!!')
 
 StartupEvents.registry('item', event => {
-	var pokeballs = ['poke','citrine','verdant','azure','roseate','slate','premier','great','ultra','safari','fast','level','lure','heavy','love','friend','moon','sport','park','net','dive','nest','repeat','timer','luxury','dusk','heal','quick','dream','beast','master','cherish']
+	var pokeballs = ['poke', 'citrine', 'verdant', 'azure', 'roseate', 'slate', 'premier', 'great', 'ultra', 'safari', 'fast', 'level', 'lure', 'heavy', 'love', 'friend', 'moon', 'sport', 'park', 'net', 'dive', 'nest', 'repeat', 'timer', 'luxury', 'dusk', 'heal', 'quick', 'dream', 'beast', 'master', 'cherish']
 
-    pokeballs.forEach(f => {
-        event.create(`uncharged_${f}_ball`).texture(`cobblemon:items/poke_balls/${f}_ball`).color(0, 0x4f5359);
-        event.create(`incomplete_${f}_ball`, `create:sequenced_assembly`).texture(`cobblemon:items/poke_balls/${f}_ball`).color(0, 0x222324);
-    });
-	
+	pokeballs.forEach(f => {
+		event.create(`uncharged_${f}_ball`).texture(`cobblemon:items/poke_balls/${f}_ball`).color(0, 0x4f5359);
+		event.create(`incomplete_${f}_ball`, `create:sequenced_assembly`).texture(`cobblemon:items/poke_balls/${f}_ball`).color(0, 0x222324);
+	});
+
 	var _type = ['Ingot', 'Nugget', 'Sheet']
 	_type.forEach(f => {
 		event.create(`platinum_${f.toLowerCase()}`).texture(`create:item/brass_${f.toLowerCase()}`).color(0, 0x85a69e).displayName(`Platinum ${f}`);
 	})
+
+	var apricorns = ['red', 'yellow', 'green', 'blue', 'pink', 'black', 'white']
+
+	apricorns.forEach(f => {
+		event.create(`cooked_${f}_apricorn`).color(0, 0x4f5359);
+	});
+
+	event.create('incomplete_metal_coating', 'create:sequenced_assembly').texture('cobblemon:items/metal_coat').color(o, 0x222324)
+	event.create('unwrapped_candy', 'create:sequenced_assembly')
+
+	event.create('crushed_platinum_ore')
+	event.create('unwrapped_candy')
+	event.create('ruby')
+
+	event.create('randomium_ingot')
+	event.create('randomium_sheet')
+
+
+
+
+
+
 })
 
-	// Register new items here
-	// event.create('example_item').displayName('Example Item')
+// Register new items here
+// event.create('example_item').displayName('Example Item')
 
 StartupEvents.registry('block', event => {
 	// Register new blocks here
